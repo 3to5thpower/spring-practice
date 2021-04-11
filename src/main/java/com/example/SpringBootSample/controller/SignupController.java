e042cd54d6b97bd635d7dd0bf9e7257dd974fe5c
3to5thpower <nisi3genki@gmail.com>
2021-04-12 03:18:30 +0900

    :sparkles: implement validation

--- src/main/java/com/example/SpringBootSample/controller/SignupController.java
+++ src/main/java/com/example/SpringBootSample/controller/SignupController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +30,7 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+    public String postSignup(Model model, Locale locale, @ModelAttribute @Validated SignupForm form, BindingResult bindingResult) {
         // validation
         if (bindingResult.hasErrors()) {
             return getSignup(model, locale, form);
